ce1d858d17086045dbc2563172f360ec49a1a1cd
Andrew Huth <git@huth.me>
2023-11-19 17:50:11 -0500

    Implement checkerboard example
    https://wasmbyexample.dev/examples/reading-and-writing-graphics/reading-and-writing-graphics.assemblyscript.en-us.html

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,54 @@
-// The entry file of your WebAssembly module.
+// Grow memory by 1 page (64kb)
+memory.grow(1);
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+const CHECKERBOARD_SIZE: i32 = 20;
+
+export const CHECKERBOARD_BUFFER_POINTER: i32 = 0;
+export const CHECKERBOARD_BUFFER_SIZE: i32 = CHECKERBOARD_SIZE * CHECKERBOARD_SIZE * 4;
+
+export function generateCheckerBoard(
+  darkValueRed: i32,
+  darkValueGreen: i32,
+  darkValueBlue: i32,
+  lightValueRed: i32,
+  lightValueGreen: i32,
+  lightValueBlue: i32,
+): void {
+  for (let x: i32 = 0; x < CHECKERBOARD_SIZE; x++) {
+    for (let y: i32 = 0; y < CHECKERBOARD_SIZE; y++) {
+      let isDarkSquare: boolean = true;
+
+      if (y % 2 === 0) {
+        isDarkSquare = false;
+      }
+
+      if (x % 2 === 0) {
+        isDarkSquare = !isDarkSquare;
+      }
+
+      let squareValueRed = darkValueRed;
+      let squareValueGreen = darkValueGreen;
+      let squareValueBlue = darkValueBlue;
+
+      if (!isDarkSquare) {
+        squareValueRed = lightValueRed;
+        squareValueGreen = lightValueGreen;
+        squareValueBlue = lightValueBlue;
+      }
+
+      // Calculate 1d index for our memory from the 2d checkboard.
+      let squareNumber = y * CHECKERBOARD_SIZE + x;
+      let squareRgbaIndex = squareNumber * 4;
+
+      // Store the values.
+      // Red
+      store<u8>(CHECKERBOARD_BUFFER_POINTER + squareRgbaIndex + 0, squareValueRed);
+      // Green
+      store<u8>(CHECKERBOARD_BUFFER_POINTER + squareRgbaIndex + 1, squareValueGreen);
+      // Blue
+      store<u8>(CHECKERBOARD_BUFFER_POINTER + squareRgbaIndex + 2, squareValueBlue);
+      // Alpha, which is always opague
+      store<u8>(CHECKERBOARD_BUFFER_POINTER + squareRgbaIndex + 3, 255);
+    }
+  }
 }
